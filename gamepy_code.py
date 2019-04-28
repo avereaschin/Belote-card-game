66ef3735cfe66a531b39933c30ff63222c6cda85
unibookworm1 <34988834+unibookworm1@users.noreply.github.com>
2019-04-28 21:18:05 +0100

    WIP title screen and card table formatting

--- gamepy_code.py
+++ gamepy_code.py
@@ -1,17 +1,18 @@
 import pygame
 import os
 import time
-from card_vars import ranks, suits, card
+from card_vars import ranks, suits, card, deck
 import threading
 from thread_test import q, inputFunc	
 import queue
+from random import choice
 
 os.chdir(r'Pics')
 
 pygame.init()
 
-display_width = 1200
-display_height = 800
+display_width = 1280
+display_height = 720
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -32,65 +33,33 @@ for i in card_pics:
         split_suit = split[-1].split('.')[0]
         card_pic_dict[card(Rank=split_rank, Suit=split_suit)] = i
 
+# test hand 
 hand = [card(Rank='A', Suit='Clubs'), card(Rank='J', Suit='Hearts'), card(Rank='7', Suit='Spades'), card(Rank='8', Suit='Clubs'), card(Rank='10', Suit='Diamonds')]
+
+# list of loaded card images
 hand_load_pics = [pygame.image.load(card_pic_dict[i]) for i in hand]
 
+hand_load_pics = [pygame.transform.scale(i, (88, 120)) for i in hand_load_pics]
 
-
+# add rects to each card
 hand_rects = [i.get_rect() for i in hand_load_pics]
-xy_coords = [(300 + (87 + 5) * i, 680) for i in range(1, len(hand) + 1)]
+
+def findMargin(hand):
+	return (display_width / 2 - ((88 * len(hand) + 6 * (len(hand) - 1)) / 2))
+
+# (x, y) coordinates for each card
+xy_coords = [(findMargin(hand) + (88 + 6) * i, display_height - 120) for i in range(len(hand))]
+print(xy_coords)
 
 for i, j in zip(hand_rects, xy_coords):
 	i.topleft = (j)
 
 
-def resizePicDimensions(image, width=None, height=None):
-
-	if width and not height:
-		return width, int((width * pygame.Surface.get_height(image)) / pygame.Surface.get_width(image))
-	elif not width and height:
-		return int((height * pygame.Surface.get_width(image)) / pygame.Surface.get_height(image)), height
-	elif width and height:
-		return width, height
-
-
-#card_resolution = resizePicDimensions(card_img, height=75)
-#card_img = pygame.transform.scale(card_img, card_resolution)
-# rect = card_img.get_rect()
-# rect_other = [i.get_rect() for i in card_other]
-
-# other_coords = [(0, 400), (600, 0), (1200 - 87, 400)]
-# new_coords = [(300, 400), (600, 200), (1200 - 300 - 87, 400)]
-
-# for i, j in zip(rect_other, other_coords):
-# 	i.topleft = (j)
-
-# rect.center = (int(pygame.Surface.get_width(card_img) / 2), int(pygame.Surface.get_height(card_img) / 2))
 
 
 def card(x, y):
 	game_display.blit(card_img, (x, y))
 
-
-# x = display_width * 0.1
-# y = display_height - pygame.Surface.get_height(card_img)
-
-# rect.topleft = ((x, y))
-
-# print('rect center is: ', rect.center)
-
-# xy_coords = [(x + pygame.Surface.get_width(card_img) * i + 20 * i, y) for i in range(1, 6)]
-# print(xy_coords)
-
-# rects_ = [card_img.get_rect() for i in range(1, 6)]
-# for i, j in zip(rects_, xy_coords):
-# 	i.topleft = (j)
-
-# Window is closed 
-
-
-
-# turn = False
 
 # UPDATE_LATER = pygame.USEREVENT + 1
 
@@ -103,31 +72,30 @@ def title_screen():
 	intro = True
 
 	crashed = False
-	message = ''
+	# message = ''
 
 	game_display.fill(white)
 
-	while intro:
+	while not crashed:
 
 		mx, my = pygame.mouse.get_pos()	
 
-		try:
-			message = q.get(False)		
-		except queue.Empty:
-			pass
+		# try:
+		# 	message = q.get(False)		
+		# except queue.Empty:
+		# 	pass
 
-		if message:
-			return mainLoop()
+		# if message:
+		# 	return mainLoop()
 
 		rect_drawing1 = pygame.draw.rect(game_display, black, [(display_width / 2 - 100), display_height - 100, 200, 100])
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()
+				crashed = True
 			
-			# if event.type == pygame.MOUSEBUTTONDOWN and rect_drawing1.collidepoint((mx, my)):
-			# 	intro = False
+			if event.type == pygame.MOUSEBUTTONDOWN and rect_drawing1.collidepoint((mx, my)):
+				return mainLoop()
 
 		text_font = pygame.font.Font('freesansbold.ttf', 115)
 		TextSurf, TextRect = text_objects('Belote', text_font)
@@ -159,26 +127,14 @@ def mainLoop():
 			if event.type == pygame.QUIT:
 				crashed = True
 
-		
-		for i, j in zip(hand_load_pics, hand_rects):
-			game_display.blit(i, j)
+			# on left click
+			for i, j in zip(hand_load_pics, hand_rects):
+				if event.type == pygame.MOUSEBUTTONDOWN and j.collidepoint((mx, my)):
+					clicked = True
+					val = [i, j]		
+				if clicked and event.type == pygame.MOUSEBUTTONUP:
+					clicked = False
 
-
-
-
-
-		# 	# on left click
-		# 	for i in rects_:
-		# 		if event.type == pygame.MOUSEBUTTONDOWN and i.collidepoint((mx, my)):
-		# 			clicked = True
-		# 			val = i
-
-		# 	for j in rect_other:
-		# 		if event.type == pygame.MOUSEBUTTONDOWN and j.collidepoint((mx, my)):
-		# 			turn = True
-		# 			oval = j
-		# 			oval.x = new_coords[rect_other.index(oval)][0]
-		# 			oval.y = new_coords[rect_other.index(oval)][1]
 
 		# 	# on letting go of left click
 		# 	if clicked and event.type == pygame.MOUSEBUTTONUP:
@@ -199,6 +155,17 @@ def mainLoop():
 		
 		# rect_drawing = pygame.draw.rect(game_display, black, [400, 300, 400, 200], 2)
 
+		if clicked:
+			for i in hand_load_pics:
+				game_display.blit(val[0], (mx, my))
+			for i, j in zip([k for k in hand_load_pics if k != val[0]], [l for l in hand_rects if l != val[1]]):
+				game_display.blit(i, j)
+		
+
+		else:
+			for i, j in zip(hand_load_pics, hand_rects):
+				game_display.blit(i, j)
+
 
 		# if turn == True:
 
@@ -211,8 +178,7 @@ def mainLoop():
 		# 	for i, j in zip(card_other, rect_other):
 		# 		game_display.blit(i, j)
 
-		# if clicked:
-		# 	game_display.blit(card_img, (mx, my))
+		
 
 		# 	for j in [i for i in rects_ if i != val]:
 		# 		game_display.blit(card_img, j)
@@ -236,9 +202,9 @@ def mainLoop():
 	pygame.quit()
 	quit()
 
-t2 = threading.Thread(target = inputFunc)
+# t2 = threading.Thread(target = inputFunc)
 
-t2.start()
+# t2.start()
 
 print('lets go')
 
